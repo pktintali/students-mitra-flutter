a771b762414728e15a0e53aa4b2db371b8b5fec1
pktintali <pktintali@users.noreply.github.com>
2021-01-05 17:01:56 +0530

    bar graph updated

--- lib/Home/graphs.dart
+++ lib/Home/graphs.dart
@@ -1,7 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:students_mitra_flutter/Profile/profile.dart';
-import 'dart:math';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
 class Graphs extends StatefulWidget {
@@ -13,18 +14,20 @@ class Graphs extends StatefulWidget {
 class _GraphsState extends State<Graphs> {
   List<charts.Series> subValue;
   List<charts.Series> lineValue;
+  // List<String> activeSubjects;
+  CollectionReference profile =
+      FirebaseFirestore.instance.collection('usersData');
+  final user = FirebaseAuth.instance.currentUser.email;
 
-  static List<charts.Series<GraphVal, String>> _createRandomData() {
-    final random = Random();
-
-    final desktopSalesData = [
-      GraphVal('DBMS', random.nextInt(100)),
-      GraphVal('COI', random.nextInt(100)),
-      GraphVal('DS', random.nextInt(100)),
-      GraphVal('ML', random.nextInt(100)),
-      GraphVal('AI', random.nextInt(100)),
+  static List<charts.Series<GraphVal, String>> _createRandomData(
+      {List<dynamic> activeSub, Map<String, dynamic> marks}) {
+    List<GraphVal> desktopSalesData = [
+      // GraphVal('DBMS', random.nextInt(100)),
     ];
-
+    for (var i in activeSub) {
+      desktopSalesData.add(GraphVal(i.toString(),
+          10 * (marks[i].reduce((a, b) => a + b) / marks[i].length).round()));
+    }
     return [
       charts.Series<GraphVal, String>(
         id: 'Sales',
@@ -38,13 +41,13 @@ class _GraphsState extends State<Graphs> {
     ];
   }
 
-  @override
-  void initState() {
-    super.initState();
-    subValue = _createRandomData();
-  }
+  // @override
+  // void initState() {
+  //   super.initState();
+  //   subValue = _createRandomData();
+  // }
 
-  barChart() {
+  Widget barChart() {
     return charts.BarChart(
       subValue,
       animate: true,
@@ -101,7 +104,27 @@ class _GraphsState extends State<Graphs> {
                 width: MediaQuery.of(context).size.width,
                 height: MediaQuery.of(context).size.height * 0.5,
                 padding: EdgeInsets.all(20.0),
-                child: barChart(),
+                // child: barChart(),
+                child: FutureBuilder(
+                  future: profile.doc('$user').get(),
+                  builder: (BuildContext context,
+                      AsyncSnapshot<DocumentSnapshot> snapshot) {
+                    if (snapshot.hasError) {
+                      return Text(snapshot.error);
+                    } else {
+                      if (snapshot.connectionState == ConnectionState.done) {
+                        Map<String, dynamic> data = snapshot.data.data();
+                        subValue = _createRandomData(
+                            activeSub: data['activeSubject'],
+                            marks: data['marks']);
+                        // data['profile']['name'],
+                        // GraphVal('AI', random.nextInt(100)),
+                        return barChart();
+                      }
+                      return Center(child: CircularProgressIndicator());
+                    }
+                  },
+                ),
               ),
             ),
             SizedBox(
